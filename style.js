8d887bf4ac42459dfa5e10bdd6f15531f00793f8
akhtarjass <akhtarjass@gmail.com>
2020-06-14 15:43:06 +0500

    chapter 1 done

---
+++ style.js
@@ -0,0 +1,65 @@
+// chapter 1
+
+    // alert("Error! Please enter a valid password.")
+
+    // alert("Welcome to JS Land..  \n Happy Coding!")
+
+    // alert("Welcome to JS Land")
+
+    
+    // alert("Hello... I can run JS through my web browser`s console")
+
+
+
+
+    // chapter 2
+    // task1
+    // var username = "";
+
+    // task 2
+    // var myName = "akhtar hussain";
+ 
+    //TASK 3 
+    // var    massage = "Hello World";
+    // alert(massage);
+
+    // task  4
+    // var name = prompt("Enter Your Name");
+    // var age = prompt("Enter Your Age");
+    // if(name  == "" || age ==""){
+    //     alert("Enter Correct Information")
+    // }else{
+    //     alert("Certified Mobile Application Development")
+    // }
+
+    // task 5
+
+
+
+    // task 6
+    // var email = "akhtarjass@gmail.com";
+
+    // alert("My email address is "+ email)
+
+    // task 7 
+    // var book = "A smarter way to learn JavaScript.";
+
+    // alert("I am trying to learn from the Book " + book)
+
+
+    // task 8
+    // document.write("Yeh! I can write HTML content through JavaScript")
+
+    // task 9 
+    // var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+    // alert(design)
+
+
+
+
+
+    // Chapter 3
+    
+    // task 1
+    
+ 
